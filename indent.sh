84ca32d2f5a9173e703f62f8da42d55913bf356e
yomin.cheng <yomin.cheng@gmail.com>
2014-10-25 15:33:27 +0800

    config: Add config file when install new Ubuntu system
    Signed-off-by: yomin.cheng <yomin.cheng@gmail.com>

--- indent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo sync;
-echo "run indent";
-dos2unix *.c
-dos2unix *.h
-indent -i4 -bli0 -nut -npsl -npcs -br -ce -lp -cli4 -il0 -l80 *.c;
-indent -i4 -bli0 -nut -npsl -npcs -br -ce -lp -cli4 -il0 -l80 *.h;
-sudo rm -rf *~;
-sudo sync;
